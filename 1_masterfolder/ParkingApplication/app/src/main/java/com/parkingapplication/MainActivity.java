d38feab85441fd38da8ac4a6446137300ed447b8
isemang <minjin1997@naver.com>
2019-11-17 19:22:02 +0900

    [2019.11.17] 수정사항 - 박민진 - Camera 성능향상위해 CameraPreview에서 Camera2로 변경.

--- 1_masterfolder/ParkingApplication/app/src/main/java/com/parkingapplication/MainActivity.java
+++ 1_masterfolder/ParkingApplication/app/src/main/java/com/parkingapplication/MainActivity.java
@@ -1,18 +1,16 @@
 package com.parkingapplication;
 
 
-import android.app.ActionBar;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
-import android.os.Build;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
-import android.os.Environment;
-import android.view.SurfaceHolder;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
@@ -23,28 +21,41 @@ import com.parkingapplication.networks.listener.ActionResultListener;
 import com.parkingapplication.networks.network.NetworkRequestTest;
 import com.parkingapplication.utils.Logger;
 import com.parkingapplication.utils.MoveActivityUtil;
-import com.parkingapplication.view.CameraPreview;
+import com.parkingapplication.view.Camera2;
 
-import java.io.File;
-
-public class MainActivity extends BaseActivity implements View.OnClickListener {
-
-    private static CameraPreview surfaceView;
-    private static Camera mCamera;
-    public static MainActivity getInstance;
-
-    private SurfaceHolder holder;
+public class MainActivity extends BaseActivity implements View.OnClickListener , Camera2.Camera2Interface, TextureView.SurfaceTextureListener {
 
     // [s] 시크릿 페이지 관련 변수
     private int mSecretClickCnt = 0;
     private long mSecretTime = -1;
     // [e] 시크릿 페이지 관련 변수
 
+    private TextureView mTextureView;
+    private Camera2 mCamera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         MoveActivityUtil.getInstance().moveIntroActivity(mActivity);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mTextureView != null){
+            if(mTextureView.isAvailable()){
+                openCamera();
+            } else {
+                mTextureView.setSurfaceTextureListener(this);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        closeCamera();
+        super.onPause();
     }
 
     //ctrl+i
@@ -85,31 +96,17 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         }
     }
 
-    public static Camera getCamera() {
-        return mCamera;
-    }
-
     private void setInit() {
-        getInstance = this;
-
-        // 카메라 객체를 R.layout.activity_main의 레이아웃에 선언한 SurfaceView에서 먼저 정의해야 함으로 setContentView 보다 먼저 정의한다.
-        mCamera = Camera.open();
-
         setContentView(R.layout.activity_main);
 
         // 함수 내에서 초기화 할수 있는 영역
         findViewById(R.id.img_logo).setOnClickListener(this);
 
+        // 클래스 내에서 초기화 해야 하는 영역
+        mTextureView = findViewById(R.id.v_texture);
+        mTextureView.setSurfaceTextureListener(this);
 
-        // 클래스 내에서 초기화 할수 있는 영역
-        // SurfaceView를 상속받은 레이아웃을 정의한다.
-        surfaceView = (CameraPreview) findViewById(R.id.camera);
-
-
-        // SurfaceView 정의 - holder와 Callback을 정의한다.
-        holder = surfaceView.getHolder();
-        holder.addCallback(surfaceView);
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        mCamera = new Camera2(mContext,this);
 
         // Test API Call.
         NetworkManager.getInstance().add(new NetworkRequestTest(mContext, new ActionResultListener<TestModel>() {
@@ -123,6 +120,54 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 Logger.d("onFail\t" + error);
             }
         })).runNext();
+    }
 
+    private void openCamera(){
+        CameraManager cameraManager = mCamera.CameraManager_1(mActivity);
+        String cameraId = mCamera.CameraCharacteristics_2(cameraManager);
+        mCamera.CameraDevice_3(cameraManager,cameraId);
+    }
+
+    private void closeCamera(){
+        if(mCamera != null){
+            mCamera.closeCamera();
+        }
+    }
+
+    // [s] TextureView.SurfaceTextureListener
+    //이부분 절대 절대 절대 절대 건드리지말기!!
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        openCamera();
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+    // [e] TextureView.SurfaceTextureListener
+
+    /**
+     * Camera2 Interface
+     * @param cameraDevice CameraDevice.
+     * @param cameraSize    CameraSize
+     */
+    @Override
+    public void onCameraDeviceOpened(CameraDevice cameraDevice, Size cameraSize) {
+        SurfaceTexture texture = mTextureView.getSurfaceTexture();
+        texture.setDefaultBufferSize(cameraSize.getWidth(), cameraSize.getHeight());
+        Surface surface = new Surface(texture);
+        mCamera.CaptureSession_4(cameraDevice, surface);
+        mCamera.CaptureRequest_5(cameraDevice, surface);
     }
 }
