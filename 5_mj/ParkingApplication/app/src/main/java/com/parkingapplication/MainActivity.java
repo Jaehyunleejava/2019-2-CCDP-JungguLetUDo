98a80a79ea8f1bf728c25e7b28d24604f17e707f
isemang <a55306039@gmail.com>
2019-11-30 19:05:12 +0900

    [2019.11.30]수정사항 -박민진 - Yolo 훨씬 성능 좋은거 ParkingApplication에 입히기 완료.

--- 5_mj/ParkingApplication/app/src/main/java/com/parkingapplication/MainActivity.java
+++ 5_mj/ParkingApplication/app/src/main/java/com/parkingapplication/MainActivity.java
@@ -1,29 +1,38 @@
 package com.parkingapplication;
 
-
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
+
 import androidx.annotation.Nullable;
-import com.parkingapplication.activity.BaseActivity;
+
+import com.parkingapplication.utils.Logger;
 import com.parkingapplication.utils.MoveActivityUtil;
 
-import org.tensorflow.demo.CameraConnectionFragment;
+import org.tensorflow.demo.Classifier;
+import org.tensorflow.demo.DetectorActivity;
+import org.tensorflow.demo.YoloListener;
 
-public class MainActivity extends BaseActivity {
+import java.util.List;
+
+public class MainActivity extends DetectorActivity implements YoloListener {
+
+    private Activity mActivity;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        MoveActivityUtil.getInstance().moveIntroActivity(mActivity);
-    }
-
-    private void setFragment(){
-        getFragmentManager()
-                .beginTransaction()
-                .replace(R.id.container, CameraConnectionFragment.newInstance())
-                .commit();
+        mActivity = this;
+        mContext = this;
+        setInit();
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+//        MoveActivityUtil.getInstance().moveIntroActivity(mActivity);
     }
 
     @Override
@@ -37,7 +46,26 @@ public class MainActivity extends BaseActivity {
     }
 
     private void setInit() {
-        setContentView(R.layout.activity_main);
-        setFragment();
-       }
+//        setContentView(R.layout.activity_main);
+        Logger.d("TEST:: ¿©±â±îÁö¿È??");
+        setYoloListener(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_main, null);
+        RelativeLayout.LayoutParams layoutParams =
+                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        addContentView(view, layoutParams);
+    }
+
+
+
+    @Override
+    public void onDetectedList(List<Classifier.Recognition> list) {
+
+    }
+
+    @Override
+    public void onDetected(Classifier.Recognition recognition) {
+        Logger.d("Data" + recognition.toString());
+    }
 }
